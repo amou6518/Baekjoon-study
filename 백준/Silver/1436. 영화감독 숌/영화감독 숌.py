2396cbe4a9675384073e511c8aa8e7b8e4a83dc2
이하은 <112817806+amou6518@users.noreply.github.com>
2026-04-02 01:57:59 +0900

    [Silver V] Title: 영화감독 숌, Time: 660 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+target = 666
+cnt = 0
+
+while True:
+    if '666' in str(target):
+        cnt += 1 
+    if cnt == n:
+        print(target)
+        break
+    target += 1

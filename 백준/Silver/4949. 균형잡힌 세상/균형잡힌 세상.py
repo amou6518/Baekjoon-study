d5fb7e211ec3ffcc3381185fa5390c6da315b422
이하은 <112817806+amou6518@users.noreply.github.com>
2026-04-11 15:13:11 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,31 @@
+import sys
+
+while True:
+    line = sys.stdin.readline().rstrip('\n')
+    
+    if line == ".":
+        break
+    
+    stack = []
+    is_balanced = True
+    
+    for char in line:
+        if char == '(' or char == '[':
+            stack.append(char)
+        
+        elif char == ')':
+            if not stack or stack[-1] != '(':
+                is_balanced = False
+                break
+            stack.pop()
+            
+        elif char == ']':
+            if not stack or stack[-1] != '[':
+                is_balanced = False
+                break
+            stack.pop()
+            
+    if is_balanced and not stack:
+        print("yes")
+    else:
+        print("no")
